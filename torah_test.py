452e231ec2fec0ea3b570b968c4859fb02b6a117
yishai <yishairasowsky@gmail.com>
2020-12-02 19:45:06 +0200

    create sample app create input for test app

--- torah_test.py
+++ torah_test.py
@@ -11,148 +11,192 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
+    return HOME_HTML
 
-    to_display = ''
-    
-    name = request.args['name']
-
-    HELLO_HTML = """
+HOME_HTML = """
     <html><body>
-    The time is {1}.
-    <h1>Hello, {0}!</h1>
-    <hr>
+        <h2>Welcome to A.I. Testing<br>
+        Automatically Generated Questions
+        </h2>
+        <form action="/verify">
+            What's your name? <input type='text' name='username'>
+                <br>
+                <br>
+            What do you want to study? <input type='text' name='to_study'>
+                <br>
+                <br>
+            <input type='submit' value='Submit'>
+        </form>
+
     </body></html>"""
 
-    to_display += HELLO_HTML.format(name, str(datetime.now()))
+@app.route('/verify')
+def verify():
 
-    # url = 'http://www.sefaria.org/api/texts/Berakot.2a.10-13'
+    username = request.args.get('username', 'World')
+    to_study = request.args.get('to_study', '')
 
-    # research_item = 'Rashi'
-    research_item = 'Rambam'
-
-    url = f'https://en.wikipedia.org/api/rest_v1/page/summary/{research_item}'
-
-    r = requests.get(url)
-    obj = r.json()
-
-
-    key = 'extract'
-    content = obj[key]
-
-    html_str = content
-    soup = BeautifulSoup(html_str,features="html.parser")
-    text = soup.get_text()
-
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(text)
+    if to_study == '':
+        msg = 'You did not tell me what you want to study.'
     
-    ents_dict = {
-        'word':[ent.text for ent in doc.ents],
-        'label':[ent.label_ for ent in doc.ents],
-    }
+    else:
+        msg = f'Are you sure you want to study <b>{to_study}</b>?'
 
-    ents_df = pd.DataFrame(ents_dict)
-    ents_df
+    return VERIFY_HTML.format(username, msg)
 
-    unique_ents_df = ents_df.drop_duplicates()
-    unique_ents_dict = {}
-    
-    for row in unique_ents_df.itertuples():
-        unique_ents_dict[row[1]] = row[2]
+VERIFY_HTML = """
+    <html><body>
+        <h2>Hello, {0}!</h2>
+        {1}
+        <form>
+            <button type="submit" formaction="/learn">Study now</button>
+        </form>
+
+    </body></html>
+    """
+
+@app.route('/learn')
+def learn():
+
+    username = request.args.get('username', 'World')
+    to_study = request.args.get('to_study', 'no topic chosen')
+
+    return STUDY_HTML.format(username,to_study)
 
 
-    to_display += '<h1>Material to Learn:</h1> '
-    to_display += text
+STUDY_HTML = """
+    <html><body>
+        <h2>Hello, {0}!</h2>
+        Now you will really study: {1}
 
-    to_display += '<hr>'
-    to_display += '<hr>'
-    to_display += '<h1>Study Questions:</h1> '
-    to_display += '<h3>(#1) Wh- questions:</h3> '
+    </body></html>
+    """
+
+    # url = f'https://en.wikipedia.org/api/rest_v1/page/summary/{research_item}'
+
+    # r = requests.get(url)
+    # obj = r.json()
 
 
-    idx = 1
+    # key = 'extract'
+    # content = obj[key]
+
+    # html_str = content
+    # soup = BeautifulSoup(html_str,features="html.parser")
+    # text = soup.get_text()
+
+    # nlp = spacy.load("en_core_web_sm")
+    # doc = nlp(text)
     
-    for token,label in unique_ents_dict.items():
+    # ents_dict = {
+    #     'word':[ent.text for ent in doc.ents],
+    #     'label':[ent.label_ for ent in doc.ents],
+    # }
+
+    # ents_df = pd.DataFrame(ents_dict)
+    # ents_df
+
+    # unique_ents_df = ents_df.drop_duplicates()
+    # unique_ents_dict = {}
     
-        to_display+=f"({idx}) "
+    # for row in unique_ents_df.itertuples():
+    #     unique_ents_dict[row[1]] = row[2]
+
+
+    # to_display += '<h1>Material to Learn:</h1> '
+    # to_display += text
+
+    # to_display += '<hr>'
+    # to_display += '<hr>'
+    # to_display += '<h1>Study Questions:</h1> '
+    # to_display += '<h3>(#1) Wh- questions:</h3> '
+
+
+    # idx = 1
     
-        if label in {'ORG','PRODUCT'}:
-            to_display+=f"What is meant by {token}?<br>"
+    # for token,label in unique_ents_dict.items():
     
-        elif label in {'PERSON',''}:
-            to_display+=f"Who is {token}?<br>"
+    #     to_display+=f"({idx}) "
     
-        elif label=='DATE':
-            to_display+=f"What occurred at the time of {token}?<br>"
+    #     if label in {'ORG','PRODUCT'}:
+    #         to_display+=f"What is meant by {token}?<br>"
     
-        elif label in {'LOC','GPE'}:
-            to_display+=f"What occurred in {token}?<br>"
+    #     elif label in {'PERSON',''}:
+    #         to_display+=f"Who is {token}?<br>"
+    
+    #     elif label=='DATE':
+    #         to_display+=f"What occurred at the time of {token}?<br>"
+    
+    #     elif label in {'LOC','GPE'}:
+    #         to_display+=f"What occurred in {token}?<br>"
         
-        else:
-            to_display+=f"<b>*</b> No question generated for: <b>{token}</b><br>"
+    #     else:
+    #         to_display+=f"<b>*</b> No question generated for: <b>{token}</b><br>"
 
-        idx += 1
+    #     idx += 1
 
-    text_with_blanks = text
-    for ent in doc.ents:
-        text_with_blanks = f'{text_with_blanks[:ent.start_char]}{"_"*(ent.end_char-ent.start_char)}{text_with_blanks[ent.end_char:]}'
+    # text_with_blanks = text
+    # for ent in doc.ents:
+    #     text_with_blanks = f'{text_with_blanks[:ent.start_char]}{"_"*(ent.end_char-ent.start_char)}{text_with_blanks[ent.end_char:]}'
 
 
 
-    to_display += '<hr>'
-    to_display += '<h3>(#2) Fill in the blank:</h3> '
-    to_display += text_with_blanks
+    # to_display += '<hr>'
+    # to_display += '<h3>(#2) Fill in the blank:</h3> '
+    # to_display += text_with_blanks
     
 
-    true_sents = []
-    false_sents = []
+    # true_sents = []
+    # false_sents = []
 
-    words_to_replace = {'not','is','was'}
+    # words_to_replace = {'not','is','was'}
 
-    for sent in doc.sents:
+    # for sent in doc.sents:
 
-        sent_text_true = sent.text
-        sent_text_false = 'NA'
+    #     sent_text_true = sent.text
+    #     sent_text_false = 'NA'
 
-        if any(word_to_replace in sent_text_true for word_to_replace in words_to_replace):
+    #     if any(word_to_replace in sent_text_true for word_to_replace in words_to_replace):
         
-            sent_text_false = sent_text_true.replace('not ',' ',1)
-            sent_text_false = sent_text_true.replace('is','is not',1)
-            sent_text_false = sent_text_true.replace('was','was not',1)
+    #         sent_text_false = sent_text_true.replace('not ',' ',1)
+    #         sent_text_false = sent_text_true.replace('is','is not',1)
+    #         sent_text_false = sent_text_true.replace('was','was not',1)
 
-            true_sents.append(sent_text_true)
-            false_sents.append(sent_text_false)
+    #         true_sents.append(sent_text_true)
+    #         false_sents.append(sent_text_false)
 
-    tf_dict = {
-        'true':true_sents,
-        'false':false_sents,
-    }
+    # tf_dict = {
+    #     'true':true_sents,
+    #     'false':false_sents,
+    # }
 
-    tf_df = pd.DataFrame(tf_dict)
+    # tf_df = pd.DataFrame(tf_dict)
 
-    tf_dict = {}
-    tf_dict['true'] = []
-    tf_dict['false'] = []
-    for row in tf_df.itertuples():
-        tf_dict['true'].append( row[1])
-        tf_dict['false'].append( row[2])
+    # tf_dict = {}
+    # tf_dict['true'] = []
+    # tf_dict['false'] = []
+    # for row in tf_df.itertuples():
+    #     tf_dict['true'].append( row[1])
+    #     tf_dict['false'].append( row[2])
 
-    tf_df = pd.DataFrame(tf_dict)
+    # tf_df = pd.DataFrame(tf_dict)
 
-    t_or_f_lst = []
-    for idx,row in enumerate(tf_df.itertuples()):
-        t_or_f = random.choice([1,2])
-        t_or_f_lst.append(f'<br>({idx + 1}) {row[t_or_f]}')
+    # t_or_f_lst = []
+    # for idx,row in enumerate(tf_df.itertuples()):
+    #     t_or_f = random.choice([1,2])
+    #     t_or_f_lst.append(f'<br>({idx + 1}) {row[t_or_f]}')
 
-    t_or_f_str = '<br>'.join(t_or_f_lst)
+    # t_or_f_str = '<br>'.join(t_or_f_lst)
     
-    to_display += '<hr>'
-    to_display += '<h3>(#3) True or False:</h3> '
-    to_display += t_or_f_str
+    # to_display += '<hr>'
+    # to_display += '<h3>(#3) True or False:</h3> '
+    # to_display += t_or_f_str
 
-    # to_display = ''
-    return to_display
+    # # to_display = ''
+    # return to_display
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    # Launch the Flask dev server
+    app.run(debug=True, host='localhost') #run app in debug mode on port 5000
+    # app.run(debug=True, port=5000) #run app in debug mode on port 5000
